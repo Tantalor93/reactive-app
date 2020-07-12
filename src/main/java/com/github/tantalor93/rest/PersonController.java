cf6d3e674602941198c7cad64eaba5349c7265c4
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2020-07-12 17:01:15 +0200

    example of blocking call

--- src/main/java/com/github/tantalor93/rest/PersonController.java
+++ src/main/java/com/github/tantalor93/rest/PersonController.java
@@ -1,5 +1,7 @@
 package com.github.tantalor93.rest;
 
+import java.util.concurrent.TimeUnit;
+
 import com.github.tantalor93.model.Person;
 import com.github.tantalor93.service.PersonService;
 import io.swagger.annotations.Api;
@@ -7,16 +9,23 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @Api(description = "Person operations")
 public class PersonController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class);
+
     private final PersonService personService;
 
     public PersonController(final PersonService personService) {
@@ -42,5 +51,25 @@ public class PersonController {
     @PostMapping("/persons")
     public Mono<Person> createPerson(@ApiParam(value = "person object to create", required = true) @RequestBody final Person person) {
         return personService.save(person);
+    }
+
+    @GetMapping("/benky")
+    public Mono<String> get() {
+        return Mono.just("ahoj")
+                .map(String::toUpperCase)
+                .flatMap(e -> sleep(e));
+    }
+
+    private Mono<String> sleep(String s) {
+        return Mono.just(s)
+                .map(e -> {
+                    try {
+                         TimeUnit.MILLISECONDS.sleep(50);
+                    } catch (InterruptedException interruptedException) {
+                        interruptedException.printStackTrace();
+                    }
+                    LOGGER.info("sleep done '{}'", s);
+                    return s + "1";
+                }).subscribeOn(Schedulers.parallel());
     }
 }
